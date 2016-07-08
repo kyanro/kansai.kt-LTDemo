299aa0fd9f0df7002e4f2c166485345108e7ccdc
kyanro <gw.fnct+github_pub@gmail.com>
2016-07-08 21:27:20 +0900

    lombok 動作確認

--- app/src/main/java/com/kyanro/ltdemo/MainActivity.java
+++ app/src/main/java/com/kyanro/ltdemo/MainActivity.java
@@ -1,13 +1,37 @@
 package com.kyanro.ltdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import lombok.Getter;
+import lombok.Setter;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView hello = (TextView) findViewById(R.id.hello_text);
+
+        MyData myData = new MyData();
+
+        myData.setHoge("Hello");
+        myData.setFuga("World");
+
+        hello.setText(myData.getHoge() + " " + myData.getFuga());
+    }
+
+    static class MyData {
+        @Getter
+        @Setter
+        private String hoge;
+        @Getter
+        @Setter
+        private String fuga;
     }
 }
