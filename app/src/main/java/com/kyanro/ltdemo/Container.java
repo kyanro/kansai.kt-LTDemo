71beb73fca4ba20d87d15a31a859800d1f1174f6
kyanro <gw.fnct+github_pub@gmail.com>
2016-07-09 00:31:14 +0900

    デモ1: Inlineリファクタリング用の状態

--- app/src/main/java/com/kyanro/ltdemo/Container.java
+++ app/src/main/java/com/kyanro/ltdemo/Container.java
@@ -1,5 +1,13 @@
 package com.kyanro.ltdemo;
 
 public class Container {
-    public String data;
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 }
